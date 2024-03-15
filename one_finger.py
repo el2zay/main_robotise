be9a41f79aee67e9dbc36e5690f3c0f772002644
Elie <79168733+el2zay@users.noreply.github.com>
2024-03-15 11:53:23 +0100

    Update one_finger.py

--- one_finger.py
+++ one_finger.py
@@ -1,5 +1,5 @@
 from microbit import * 
-pin0.set_analog_period(20) # M... en d' de période de 20ms 
+pin0.set_analog_period(20) # Modulation en largeur d'impulsion de période de 20ms 
                             # Sur la broche de commande n°0
 
 
@@ -8,12 +8,11 @@ def servo(angle): # Création d'une fonction "sevo" permettant la génération d
                                 # Sur la broche de commande n°0
 
 while True:
-    pot = pin2.read_analog() # 
-    print('pot = ', pot,) # Afficher cette valeur (dans la gamme allant de ... à ... ) 
+    pot = pin2.read_analog() # Capture de la valeur délivrée par le potentiomètre rotatif
+    print('pot = ', pot,) # Afficher cette valeur (dans la gamme allant de 0 à 1023 ) 
     print((pot,)) # Afficher le graphique de la valeur de la variable angle
-    angle = (pot / 1023) * 180 # Normalisation de cette valeur sur la plage ... à ...
-    print('angle = ', angle, 'Deg') # Affichage de cette valeur 
-    servo(angle) # appel de la fonction servo
-    sleep(20) # Pause de 200ms entre la prise d'échantillon, pour soulager 
+    angle = (pot / 1023) * 180 # Normalisation de cette valeur sur la plage 0 à 180
+    print('angle = ', angle, 'Deg') # Affichage de cette valeur angle en degrés
+    servo(angle) # appel de la fonction "servo"
+    sleep(20) # Pause de 20ms entre la prise d'échantillon, pour soulager 
               # la liaison série, lors des transferts d'informations.
-
