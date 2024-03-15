be33ff1c7d45394350b9a73ee1065bde8b117d81
Elie <79168733+el2zay@users.noreply.github.com>
2024-03-15 11:13:42 +0100

    Update 5.1.py

--- 5.1.py
+++ 5.1.py
@@ -1,15 +1,19 @@
-from microbit import *
-pin0.set_analog_period(20) # 
+from microbit import * 
+pin0.set_analog_period(20) # M... en d' de période de 20ms 
+                            # Sur la broche de commande n°0
+
 
 def servo(angle): # Création d'une fonction "sevo" permettant la génération du signal
-    pin0.write_analog(angle)
+    pin0.write_analog(angle) # à partir de la valeur calculée de la variable angle
+                                # Sur la broche de commande n°0
 
 while True:
-    pot = pin2.read_analog() 
-    print('pot = ', pot,)
-    print((pot,))
-    angle = (pot / 1023) * 180
-    print('angle = ', angle, 'Deg')
+    pot = pin2.read_analog() # 
+    print('pot = ', pot,) # Afficher cette valeur (dans la gamme allant de ... à ... ) 
+    print((pot,)) # Afficher le graphique de la valeur de la variable angle
+    angle = (pot / 1023) * 180 # Normalisation de cette valeur sur la plage ... à ...
+    print('angle = ', angle, 'Deg') # Affichage de cette valeur 
     servo(angle) # appel de la fonction servo
     sleep(20) # Pause de 200ms entre la prise d'échantillon, pour soulager 
               # la liaison série, lors des transferts d'informations.
+
