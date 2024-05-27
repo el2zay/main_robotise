a82ac6d6682988cbc1b52894601b7cd556740ef2
Elie <79168733+el2zay@users.noreply.github.com>
2024-05-27 18:35:14 +0200

    Update sender.py

--- code/6.1/sender.py
+++ code/6.1/sender.py
@@ -1,25 +1,17 @@
 from microbit import *
 import radio # Importation du module radio
-radio.config(group=4, power=7) # Attribution du module radio pour le gruope (ou le réseau n°1)
+radio.config(group=4, power=7) # Attribution du module radio pour le groupe 4
                                # Puissance d'émission maximale (1 mini - 7 maxi)
 radio.on() # Activation du module radio
-isStarted = False
-angle = 0    
+angle = 0 # Variable angle sur 0
 
-while True:
-    gesture = accelerometer.current_gesture()
-    if gesture == "left":
-        print("left")
-        radio.send("left")
-    if gesture == "right":
-        print("right")
-        radio.send("right")
-    if gesture == "up":
-        print("up")
-        print("up")
-        radio.send("up")
-        print("up")
-    if gesture == "down":
-        print("down")
-        radio.send("down")
-    
+while True: # Boucle infinie
+    gesture = accelerometer.current_gesture() # Détecteur de geste
+    if gesture == "left": # Si la carte est vers la gauche
+        radio.send("left") # On envoie left
+    if gesture == "right": # Si la carte est vers la droite
+        radio.send("right") # On envoie right
+    if gesture == "up": # Si la carte est vers le haut
+        radio.send("up") # On envoie up
+    if gesture == "down": # Si la carte est vers le bas
+        radio.send("down") # On envoie down
